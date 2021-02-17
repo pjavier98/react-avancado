a4a9503a35345f5f020870e704680a23aefa1872
Pedro Javier <pjpc@ic.ufal.br>
2021-02-16 23:30:21 -0300

    add option to size on logo

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -19,4 +19,20 @@ describe('<Logo />', () => {
       color: '#030517'
     })
   })
+
+  it('should render a normal logo by default', () => {
+    renderWithTheme(<Logo />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+
+  it('should render a bigger logo', () => {
+    renderWithTheme(<Logo size="large" />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
+    })
+  })
 })
